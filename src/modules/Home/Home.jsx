33707124cdb0d8a5e1093c104ff36821669046fb
unknown <nazar.bylen.ir.2021@lpnu.ua>
2024-05-07 12:23:47 +0300

    home page

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,9 +1,26 @@
+import "./style.css"
+import {useNavigate} from "react-router-dom";
+
 const Home = () => {
-  return (
-    <>
-      Home
-    </>
-  )
+    const navigate = useNavigate();
+
+    const navigateTo = (link) => {
+        navigate(link)
+    }
+
+    return (
+        <>
+            <div>
+                <div className="banner">
+                    <img className="banner-library" src="/img/banner-library.png" alt="library banner"/>
+                </div>
+                <div className="book-user-choice">
+                    <button className="choice-btn" onClick={()=>{navigateTo("/books")}}>Books</button>
+                    <button className="choice-btn" onClick={()=>{navigateTo("/users")}}>User</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home;
